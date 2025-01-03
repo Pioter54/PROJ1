fb918269930bb834e9fc01d3f82d1d76d21ed7cd
34Destiny <126720595+34Destiny@users.noreply.github.com>
2025-01-03 12:46:53 +0100

    fix(main.py): Zmieniono bibliotekę z langchain na langchain_community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
 import subprocess
 from flask import Flask, render_template, request, jsonify
